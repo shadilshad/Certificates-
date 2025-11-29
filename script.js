302d6e1121cb7cca4e4d750599e0ae85faca1913
SHADIL VV <shadilshad508@gmail.com>
2025-11-29 18:32:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    document.getElementById("academicBtn").addEventListener("click", function () {
+        window.location.href = "academic.html"; 
+    });
+
+    document.getElementById("internshipBtn").addEventListener("click", function () {
+        window.location.href = "internship.html"; 
+    });
+
+    document.getElementById("Workshop").addEventListener("click", function () {
+        window.location.href = "workshop.html";
+    });
+
+    document.getElementById("otherBtn").addEventListener("click", function () {
+        window.location.href = "other.html"; 
+    });
